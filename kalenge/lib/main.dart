62037a720e4fc43a2025e9691fdd0e8bf396eb02
Snyal <bordes.contact@gmail.com>
2020-04-07 14:29:14 +0200

    Regex 1.0 sur connexion

--- kalenge/lib/main.dart
+++ kalenge/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'register.dart';
+import 'form/register.dart';
+import 'form/formCheck.dart';
+import 'theme.dart' as ThemeApp;
+
 
 void main() => runApp(MyApp());
 
@@ -9,21 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      title: 'Kalenge',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xefefefFF),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      title: 'KalengeTheme',
+      theme: ThemeApp.appTheme,
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
         '/register': (BuildContext context) => new register()
@@ -39,118 +29,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+  String _email;
+  String _passWord;
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      resizeToAvoidBottomInset: false,
-      body:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Stack(
+
+    return new MaterialApp(
+
+      home: new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+              Container(
+                padding:EdgeInsets.fromLTRB(15.0, 100.0, 0.0, 0.0),
+                child: Text('Kalenge',
+                style: Theme.of(context).textTheme.headline),
+              ),
+
+              Container(
+                margin: new EdgeInsets.only(top: 75.0, left: 20.0, right: 20.0),
+                child: new Form(
+                  key: _formKey,
+                  autovalidate: _autoValidate,
+                  child: FormUI(),
+                ),
+              ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  padding:EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text('Kalenge',
-                      style:TextStyle(
-                          fontSize: 60.0,
-                          fontWeight: FontWeight.bold,
-                          color:Colors.redAccent)),
+                Text(
+                  'Pas de compte ?',
+                  style:  Theme.of(context).textTheme.body2,
+                ),
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    //Navigator.of(context).pushNamed('/register');
+                  },
+                  child: Text(
+                    'Inscription',
+                    style:  Theme.of(context).textTheme.body1,
                   ),
-               ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+// Formulaire de connexion
+  Widget FormUI() {
+    return new Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+
+        new TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Email'
+          ),
+          keyboardType: TextInputType.emailAddress,
+          validator: validateEmail,
+          onSaved: (String val) {
+            _email = val;
+          },
+
+        ),
+
+        SizedBox(height: 20.0),
+
+        new TextFormField(
+          decoration: const InputDecoration(labelText: 'Mot De Passe'),
+
+          keyboardType: TextInputType.text,
+          validator: validatePassWord,
+          onSaved: (String val) {
+            _passWord = val;
+          },
+        ),
+
+        SizedBox(height: 5.0),
+        Container(
+          alignment: Alignment(1.0, 0.0),
+          padding: EdgeInsets.only(top: 15.0, left: 20.0),
+          child: InkWell(
+            child: Text(
+              'Mot de passe oublié?',
+              style:  Theme.of(context).textTheme.body2
             ),
           ),
-          Container(
-              padding: EdgeInsets.only(top: 75.0, left: 20.0, right: 20.0),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'MAIL',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.redAccent)
-                           )),
-                  ),
-                  SizedBox(height: 20.0),
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'MOT DE PASSE',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey
-                            ),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.redAccent))),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 5.0),
-                  Container(
-                    alignment: Alignment(1.0, 0.0),
-                    padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                    child: InkWell(
-                      child: Text(
-                        'Mot de passe oublié?',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Montserrat',
-                            decoration: TextDecoration.underline),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Container(
-                    height: 50.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.redAccent,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Connexion',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-          SizedBox(height: 15.0),
+        ),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Pas de compte ?',
-                style: TextStyle(fontFamily: 'Montserrat'),
-              ),
-              SizedBox(width: 5.0),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed('/register');
-                },
-                child: Text(
-                  'Inscription',
-                  style: TextStyle(
-                      color: Colors.redAccent,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline),
-                ),
-              )
-            ],
-          )
-        ],
-      ));
+        new SizedBox(
+          height: 20.0,
+        ),
+        new FlatButton(
+          onPressed: _validateInputs,
+          color: Colors.red,
+          textColor: Colors.white,
+
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          padding: EdgeInsets.all(20.0),
+          child: Text(
+              'Connexion',
+              style : Theme.of(context).textTheme.button
+          ),
+
+        )
+      ],
+    );
   }
+
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+    } else {
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+  }
+
+
 }
+
